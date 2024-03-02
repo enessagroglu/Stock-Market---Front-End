264441075259a898b1525e3b1c307facaaf3c526
Enes Sağıroğlu <enes-2001sagroglu@hotmail.com>
2024-03-02 15:57:05 +0300

    first try

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
+import "/node_modules/primeflex/primeflex.css";
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import MenuBar from "./components/MenuBar";
+import { Button } from 'primereact/button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <PrimeReactProvider>
+      <>
+        <div class="grid flex">
+          <div class="col-3">
+            <MenuBar />
+          </div>
+
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </>
+    </PrimeReactProvider>
   );
 }
 
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,44 @@
+
+import React, { useRef } from 'react';
+import { Menu } from 'primereact/menu';
+import { Toast } from 'primereact/toast';
+
+export default function MenuBar() {
+    const toast = useRef(null);
+    const items = [
+        {
+            label: 'Documents',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-plus'
+                },
+                {
+                    label: 'Search',
+                    icon: 'pi pi-search'
+                }
+            ]
+        },
+        {
+            label: 'Profile',
+            items: [
+                {
+                    label: 'Settings',
+                    icon: 'pi pi-cog'
+                },
+                {
+                    label: 'Logout',
+                    icon: 'pi pi-sign-out'
+                }
+            ]
+        }
+    ];
+
+    return (
+        <div className="card flex justify-content-center">
+            <Toast ref={toast} />
+            <Menu model={items} />
+        </div>
+    )
+}
+        
