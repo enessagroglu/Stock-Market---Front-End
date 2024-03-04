b9fe284334ac113fd83f9572d8d3ab69dda00cc1
Emre Demir <emred644@gmail.com>
2024-03-04 13:48:10 +0300

    fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,19 @@
+import { Card } from 'primereact/card';
+import { Image } from 'primereact/image';
+
+
 const Contact = () => {
     return (
-      <div>
+      <Card className="iletisim" >    
         <h1>İletişim</h1>
+        <br></br>
         <p>Bize ulaşmak için aşağıdaki bilgileri kullanabilirsiniz:</p>
         <ul>
           <li>E-posta: info@example.com</li>
           <li>Telefon: +90 123 456 7890</li>
           <li>Adres: ...</li>
         </ul>
-      </div>
+      </Card>
     );
   };
   
