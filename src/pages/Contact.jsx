35be278963ce5059039b0bb5777685974d7c83b9
Emre Demir <emred644@gmail.com>
2024-03-04 16:04:49 +0300

    finished

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,40 @@
 import { Card } from 'primereact/card';
-import { Image } from 'primereact/image';
-
+import Logo from '../assets/logo.webp';
 
 const Contact = () => {
-    return (
-      <Card className="iletisim" >    
-        <h1>İletişim</h1>
-        <br></br>
-        <p>Bize ulaşmak için aşağıdaki bilgileri kullanabilirsiniz:</p>
-        <ul>
-          <li>E-posta: info@example.com</li>
-          <li>Telefon: +90 123 456 7890</li>
-          <li>Adres: ...</li>
-        </ul>
+  return (
+    <div className="flex justify-content-center">
+      <Card
+        className="flex flex-column shadow-lg" // Add shadow effect
+        style={{
+          width: '350px', // Slightly wider card
+          margin: '20px auto', // Increase vertical and horizontal spacing
+          borderRadius: '8px', // Rounded corners
+          backgroundColor: '#f2f2f2', // Lighter background
+        }}
+      >
+        <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+          <img src={Logo} alt="Contact Information" className="logoo" style={{ maxWidth: '250px', margin: '0' }} />
+        </div>
+        <div style={{ alignSelf: 'center', textAlign: 'center', padding: '15px' }}>
+          <h1 style={{ color: '#333', fontFamily: 'Roboto', fontSize: '34px', fontWeight: 'bold' }}>İletişim</h1>
+          <br />
+          <p style={{ color: '#666', fontSize: '16px' }}>Bize ulaşmak için aşağıdaki bilgileri kullanabilirsiniz:</p>
+          <ul style={{ padding: '0', listStyle: 'none' }}> 
+            <li style={{ marginBottom: '10px' }}>
+              <span style={{ color: '#444', fontSize: '18px', fontWeight: 'bold' }}>E-posta:</span> info@example.com
+            </li>
+            <li style={{ marginBottom: '10px' }}>
+              <span style={{ color: '#444', fontSize: '18px', fontWeight: 'bold' }}>Telefon:</span> +90 123 456 7890
+            </li>
+            <li>
+              <span style={{ color: '#444', fontSize: '18px', fontWeight: 'bold' }}>Adres:</span> ...
+            </li>
+          </ul>
+        </div>
       </Card>
-    );
-  };
-  
-  export default Contact;
-  
+    </div>
+  );
+};
+
+export default Contact;
