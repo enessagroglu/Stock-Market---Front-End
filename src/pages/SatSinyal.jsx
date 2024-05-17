861f8f46e7ceff3afb57c9dbb423bda6d2c3f516
Enes Sağıroğlu <enes-2001sagroglu@hotmail.com>
2024-05-17 14:29:39 +0300

    fx

--- src/pages/SatSinyal.jsx
+++ src/pages/SatSinyal.jsx
@@ -37,7 +37,7 @@ export default function SatSinyal() {
   const [filters, setFilters] = useState({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
     name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-    "country.name": { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    "bultenAdi": { value: null, matchMode: FilterMatchMode.STARTS_WITH },
     representative: { value: null, matchMode: FilterMatchMode.IN },
     status: { value: null, matchMode: FilterMatchMode.EQUALS },
     verified: { value: null, matchMode: FilterMatchMode.EQUALS },
